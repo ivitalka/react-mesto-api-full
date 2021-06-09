822260865970e7d83b11281bbfe36b016c383d6e
IVitalka <ivitalka24@gmail.com>
2021-06-09 17:33:04 +0700

    fix port 3001

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const options = {
   credentials: true,
 };
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
